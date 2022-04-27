c3976cce56cd4d6a8aea7524602bec7094f16f49
Hardanish Singh <hardanishsingh@Hardanishs-MacBook-Pro.local>
2022-04-27 16:32:17 -0400

    Sherlock and Array

--- Problem_Solving/Easy/Sherlock_and_Array.js
+++ Problem_Solving/Easy/Sherlock_and_Array.js
@@ -9,57 +9,50 @@ let inputString = '';
 let currentLine = 0;
 
 process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+        inputString += inputStdin;
 });
 
 process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
+        inputString = inputString.split('\n');
+        main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+        return inputString[currentLine++];
 }
 
 /*
- * Complete the 'balancedSums' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts INTEGER_ARRAY arr as parameter.
+ * @param { number[] } arr
+ * @return { string } flag
 */
 
-function balancedSums(arr) {
-    var leftSum = 0;
-    var rightSum = arr.slice( 1 ).reduce( (a, b) => a + b, 0);
-    var flag = "NO";
-    for( let i = 0; i < arr.length; i++ ) {
+// Complete the 'balancedSums' function below.
+
+function balancedSums( arr ) {
+        var leftSum = 0;
+        var rightSum = arr.slice( 1 ).reduce( (a, b) => a + b, 0);
+        var flag = "NO";
+        for( let i = 0; i < arr.length; i++ ) {
         if( i > 0 ) {
-            leftSum += arr[ i - 1 ];
-            rightSum -= arr[ i ];
+                leftSum += arr[ i - 1 ];
+                rightSum -= arr[ i ];
         }
         if( leftSum === rightSum ) {
-            flag = "YES";
-            break;
+                flag = "YES";
+                break;
         }
-    }
-    return flag;
+        }
+        return flag;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const T = parseInt(readLine().trim(), 10);
-
-    for (let TItr = 0; TItr < T; TItr++) {
-        const n = parseInt(readLine().trim(), 10);
-
-        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-
-        const result = balancedSums(arr);
-
-        ws.write(result + '\n');
-    }
-
-    ws.end();
+        const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+        const T = parseInt(readLine().trim(), 10);
+        for (let TItr = 0; TItr < T; TItr++) {
+                const n = parseInt(readLine().trim(), 10);
+                const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+                const result = balancedSums(arr);
+                ws.write(result + '\n');
+        }
+        ws.end();
 }
