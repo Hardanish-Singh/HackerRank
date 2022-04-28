4c6158ef93f2f1b89a1e31e9e72498c624af3a9e
Hardanish Singh <hardanishsingh1@gmail.com>
2022-04-28 10:47:17 -0400

    Diagonal Difference

--- Problem_Solving/Easy/Diagonal_Difference.js
+++ Problem_Solving/Easy/Diagonal_Difference.js
@@ -1,3 +1,36 @@
+/****************************************************************
+*               HACKERRANK DIAGONAL DIFFERENCE CHALLENGE	*
+*                                                               *
+* Given a square matrix, calculate the absolute difference      *
+* between the sums of its diagonals. For example, the square    *
+* matrix arr = [ [ 1, 2, 3], [4, 5, 6], [7, 8, 9] ]             *
+*                                                               *
+* The left-to-right diagonal = 1 + 5 + 9 = 15                   *
+* The right to left diagonal = 3 + 5 + 9 = 17                   *
+* Their absolute difference is | 15 - 17 | = 2                  *
+*                                                               *
+* Function description                                          *
+* Complete the diagonalDifference in editor below               *
+*                                                               *
+* Input Format                                                  *
+* 1) The first line contains a single integer, n, the number of *
+*    rows and columns in the square matrix arr.                 *
+* 2) Each of the next n lines describes a row, arr[i],          *
+*    and consists of n space-separated integers arr[i][j].      *
+*                                                               *
+* Constraints                                                   *
+* 1) -100 <= arr[i][j] <= 100                                   *
+*                                                               *
+* Input 1:                                                      *
+*               3                                               *
+*               11   2   4                                      *
+*               4    5   6                                      *
+*               10   8   -12                                    *
+* Output 1:                                                     *
+*               15                                              *
+*                                                               *
+****************************************************************/
+
 'use strict';
 
 const fs = require('fs');
