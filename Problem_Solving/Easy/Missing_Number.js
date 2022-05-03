f2027295d50fc0350b3884d99b8100eee0c0926c
Hardanish Singh <hardanishsingh1@gmail.com>
2022-05-03 16:34:37 -0400

    Missing Numbers

--- Problem_Solving/Easy/Missing_Number.js
+++ Problem_Solving/Easy/Missing_Number.js
@@ -1,3 +1,77 @@
+/****************************************************************
+*               HACKERRANK MISSING NUMBER CHALLENGE	        *
+*                                                               *
+* Given two arrays of integers, find which elements in the      *
+* second array are missing from the first array.                *
+*                                                               *
+* For Example: arr = [7, 2, 5, 3, 5, 3],                        *
+*              brr = [7, 2, 5, 4, 6, 3, 5, 3]                   *
+* Then, missing numbers are [4, 6]                              *
+*                                                               *
+* Notes:                                                        *
+* 1) If a number occurs multiple times in the lists, you must   *
+*    ensure that the frequency of that number in both lists is  *
+*    the same. If that is not the case, then it is also a       *
+*    missing number.                                            *
+* 2) Return the missing numbers sorted ascending.               *
+* 3) Only include a missing number once, even if it is missing  *
+*    multiple times.                                            *
+* 4) The difference between the maximum and minimum numbers in  *
+*    the original list is less than or equal to 100             *
+*                                                               *
+* Function Description: Complete the missingNumbers function in *
+* the editor below.                                             *
+*                                                               *
+* Input Format                                                  *
+* 1) The first line contains T, the number of test cases.       *
+* 2) The next T pairs of lines each represent a test case.      *
+*       2a) The first line contains n, the number of elements   *
+*           in the array arr.                                   *
+*       2b) The second line contains n space-separated integers *
+*           arr[i] where 0 <= i < n                             *
+*                                                               *
+* Constraints                                                   *
+* 1) 1 <= T <= 10                                               *
+* 2) 1 <= n <= 10^5                                             *
+* 3) 1 <= arr[i] <= 2 * 10^4                                    *
+* 4) 0 <= i < n                                                 *
+*                                                               *
+* Input 1:                                                      *
+*               2                                               *
+*               3                                               *
+*               1 2 3                                           *
+*               4                                               *
+*               1 2 3 3                                         *
+* Output 1:                                                     *
+*               NO                                              *
+*               YES                                             *
+* Explanation:                                                  *
+*       For the first test case, no such index exists.          *
+*       For the second test case, arr[0] + arr[1] = arr[3],     *
+*               therefore index 2 satisfies the given condition *
+*                                                               *
+* Input 2:                                                      *
+*               3                                               *
+*               5                                               *
+*               1 1 4 1 1                                       *
+*               4                                               *
+*               2 0 0 0                                         *
+*               4                                               *
+*               0 0 2 0                                         *
+* Output 2:                                                     *
+*               YES                                             *
+*               YES                                             *
+*               YES                                             *
+* Explanation:                                                  *
+*       For first test case, arr[2] = 4 is between two subarray *
+*               summing to 2.                                   *
+*       For second test case, arr[0] = 2 is between two         *
+*               subarrays summing to 0                          *
+*       For third test case, arr[2] = 2 is between two          *
+*               subarrays summing to 0                          *
+*                                                               *
+****************************************************************/
+
 'use strict';
 
 const fs = require('fs');
