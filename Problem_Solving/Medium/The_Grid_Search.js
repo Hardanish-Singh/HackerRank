e4afacca41aa02d675fe5010d60339b6ad4353b5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-07 07:32:40 -0400

    The Grid Search

--- Problem_Solving/Medium/The_Grid_Search.js
+++ Problem_Solving/Medium/The_Grid_Search.js
@@ -4,7 +4,12 @@
 * Given an array of strings of digits, try to find the          *
 * occurrence of a given pattern of digits. In the grid and      *
 * pattern arrays, each string represents a row in the grid.     *
-* For example, consider the following grid:                     *
+* For example, consider the following grid array:               *
+*               1234567890                                      * 
+*               0987654321                                      *
+*               1111111111                                      *
+*               1111111111                                      *
+*               2222222222                                      *
 *                                                               *
 ****************************************************************/
 
