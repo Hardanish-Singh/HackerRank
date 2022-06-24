12dbc3ec42ef2b245d8bb85f64f5c12a04b6cdd5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-24 09:54:26 -0400

    Sherlock And Anagrams

--- Problem_Solving/Medium/Sherlock_and_Anagrams.js
+++ Problem_Solving/Medium/Sherlock_and_Anagrams.js
@@ -9,28 +9,27 @@ let inputString = '';
 let currentLine = 0;
 
 process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+        inputString += inputStdin;
 });
 
 process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
+        inputString = inputString.split('\n');
+        main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+        return inputString[currentLine++];
 }
 
-/*
- * Complete the 'sherlockAndAnagrams' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts STRING s as parameter.
- */
+/** 
+ * @param { string } s
+ * @returns { number }
+*/
 
+// Complete the 'sherlockAndAnagrams' function below.
 
 function sherlockAndAnagrams( s ) {
+
     // let hash_table = { 
         
     // };
@@ -48,42 +47,37 @@ function sherlockAndAnagrams( s ) {
     // }
     // return count;
     
-    let hash_table = { 
-        
-    };
-    let count = 0;
-    for( let i = 0; i < s.length; i++ ) {
-        for ( let j = i; j < s.length; j++ ) {
-            let temp = s.slice(i, j+1).split("").sort().join("");
-            if( hash_table[temp] ) {
-                hash_table[temp] = hash_table[temp] + 1;
-            } else {
-                hash_table[ temp ] = 1;
-            }
-        } 
-    }
-    for( const[ key, value ] of Object.entries( hash_table ) ) {
-        if( value <= 1 ) {
-            continue;
-        } else {
-            count += ( ( value * ( value - 1 ) ) / 2 )
+        let hash_table = { 
+
+        };
+        let count = 0;
+        for( let i = 0; i < s.length; i++ ) {
+                for ( let j = i; j < s.length; j++ ) {
+                        let temp = s.slice(i, j+1).split("").sort().join("");
+                        if( hash_table[temp] ) {
+                                hash_table[temp] = hash_table[temp] + 1;
+                        } else {
+                                hash_table[ temp ] = 1;
+                        }
+                } 
         }
-    }
-    return count;
+        for( const[ key, value ] of Object.entries( hash_table ) ) {
+                if( value <= 1 ) {
+                        continue;
+                } else {
+                        count += ( ( value * ( value - 1 ) ) / 2 )
+                }
+        }
+        return count;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const q = parseInt(readLine().trim(), 10);
-
-    for (let qItr = 0; qItr < q; qItr++) {
-        const s = readLine();
-
-        const result = sherlockAndAnagrams(s);
-
-        ws.write(result + '\n');
-    }
-
-    ws.end();
+        const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+        const q = parseInt(readLine().trim(), 10);
+        for (let qItr = 0; qItr < q; qItr++) {
+                const s = readLine();
+                const result = sherlockAndAnagrams(s);
+                ws.write(result + '\n');
+        }
+        ws.end();
 }
