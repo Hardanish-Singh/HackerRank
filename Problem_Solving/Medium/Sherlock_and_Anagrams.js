1cc7530f35ad5284724d7e0633aee2ad6cf9cab5
Hardanish Singh <hardanishsingh1@gmail.com>
2022-06-24 10:03:25 -0400

    Sherlock And Anagrams

--- Problem_Solving/Medium/Sherlock_and_Anagrams.js
+++ Problem_Solving/Medium/Sherlock_and_Anagrams.js
@@ -1,3 +1,28 @@
+/****************************************************************
+*          HACKERRANK SHERLOCK AND ANAGRAMS CHALLENGE	        *
+*                                                               *
+* Two strings are anagrams of each other if the letters of one  *
+* string can be rearranged to form the other string. Given a    *
+* string, find the number of pairs of substrings of the string  *
+* that are anagrams of each other.                              *
+*                                                               *
+* For example, s = "mom"                                        *
+* The list of all anagrammatic pairs is [m, m], [mo, om] at     *
+* positions [ [0], [2] ], [ [0, 1], [1, 2] ] respectively       *
+*                                                               *
+* Function Description:                                         *
+* Complete the function sherlockAndAnagrams in the editor below *
+* sherlockAndAnagrams has the following parameter(s):           *
+* Input:                                                        *
+*           1) string s:                                        *
+* Returns:                                                      *
+*           1) int                                              *
+*                                                               *
+* HackerRank Problem URL:                                       *
+* https://www.hackerrank.com/challenges/sherlock-and-anagrams   *
+*                                                               *
+****************************************************************/
+
 'use strict';
 
 const fs = require('fs');
