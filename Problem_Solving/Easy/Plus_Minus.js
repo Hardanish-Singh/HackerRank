ce718b96ae83a241bbca93a26fb619b9290990f8
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-16 08:11:34 -0400

    Plus Minus

--- Problem_Solving/Easy/Plus_Minus.js
+++ Problem_Solving/Easy/Plus_Minus.js
@@ -2,26 +2,24 @@
 
 "use strict";
 
+const fs = require("fs");
+
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
 
 let inputString = "";
 let currentLine = 0;
 
-process.stdin.on("data", function (inputStdin) {
-        inputString += inputStdin;
-});
+process.stdin.on("data", (inputStdin) => (inputString += inputStdin));
 
-process.stdin.on("end", function () {
+process.stdin.on("end", () => {
         inputString = inputString.split("\n");
         main();
 });
 
-function readLine() {
-        return inputString[currentLine++];
-}
+const readLine = () => inputString[currentLine++];
 
-function plusMinus(arr) {
+const plusMinus = (arr) => {
         let minus = 0;
         let plus = 0;
         let zeros = 0;
@@ -37,13 +35,13 @@ function plusMinus(arr) {
         console.log(plus / arr.length);
         console.log(minus / arr.length);
         console.log(zeros / arr.length);
-}
+};
 
-function main() {
+const main = () => {
         const n = parseInt(readLine().trim(), 10);
         const arr = readLine()
                 .replace(/\s+$/g, "")
                 .split(" ")
                 .map((arrTemp) => parseInt(arrTemp, 10));
         plusMinus(arr);
-}
+};
