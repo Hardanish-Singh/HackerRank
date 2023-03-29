39b7f881ec1bc353a40377ebf69ee5529f053ca0
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-29 07:41:20 -0400

    Simple Array Sum

--- Problem_Solving/Easy/Simple_Array_Sum.js
+++ Problem_Solving/Easy/Simple_Array_Sum.js
@@ -1,74 +1,34 @@
-/****************************************************************
-*               HACKERRANK SIMPLE ARRAY SUM CHALLENGE	        *
-*                                                               *
-* Given an array of integers, find the sum of its elements.     *
-* For example, if the array arr = [1, 2, 3], so return 6        *
-*                                                               *
-* Function Description                                          *
-* Complete the simpleArraySum function in the editor below.     *
-* It must return the sum of the array elements as an integer.   *
-* function simpleArraySum has the following parameter(s):       *
-* arr: an array of integers                                     *
-*                                                               *
-* Input Format                                                  *
-* 1) The first line contains integer, n, denoting the size of   *
-*    the array                                                  *
-* 2) The second line contains n space-separated integers        *
-*    representing the array's elements.                         *
-*                                                               *
-* Constraints                                                   *
-* 1) 0 < n, arr[i] <= 1000                                      *
-*                                                               *
-* Output Format                                                 *
-* Print the sum of the array's elements as a single integer.    *
-*                                                               *
-* Input 1:                                                      *
-*               6                                               *
-*               1 2 3 4 10 11                                   *
-* Output 1:                                                     *
-*               31                                              *
-*                                                               *
-****************************************************************/
+// Hackerrrank: https://www.hackerrank.com/challenges/simple-array-sum/
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-        inputString += inputStdin;
-});
+process.stdin.on("data", (inputStdin) => (inputString += inputStdin));
 
-process.stdin.on('end', function() {
-        inputString = inputString.split('\n');
+process.stdin.on("end", () => {
+        inputString = inputString.split("\n");
         main();
 });
 
-function readLine() {
-        return inputString[currentLine++];
-}
+const readLine = () => inputString[currentLine++];
 
-/**
- * @param { number[] } arr
- * @return { number }
-*/
+const simpleArraySum = (arr) => arr.reduce((previousValue, currentValue) => previousValue + currentValue);
 
-// Complete the 'simpleArraySum' function below.
-
-function simpleArraySum( arr ) {
-        return arr.reduce( (previousValue, currentValue) => previousValue + currentValue );
-}
-
-function main() {
+const main = () => {
         const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
         const arCount = parseInt(readLine().trim(), 10);
-        const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+        const ar = readLine()
+                .replace(/\s+$/g, "")
+                .split(" ")
+                .map((arTemp) => parseInt(arTemp, 10));
         const result = simpleArraySum(ar);
-        ws.write(result + '\n');
+        ws.write(result + "\n");
         ws.end();
-}
+};
