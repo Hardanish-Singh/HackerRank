747a73d3edece67b8c9e433e75b982b0d2795669
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-29 08:08:00 -0400

    The Grid Search

--- Problem_Solving/Medium/The_Grid_Search.js
+++ Problem_Solving/Medium/The_Grid_Search.js
@@ -1,96 +1,51 @@
-/****************************************************************
-*               HACKERRANK THE GRID SEARCH CHALLENGE	        *
-*                                                               *
-* Given an array of strings of digits, try to find the          *
-* occurrence of a given pattern of digits. In the grid and      *
-* pattern arrays, each string represents a row in the grid.     *
-* For example, consider the following grid array:               *
-*               1234567890                                      *
-*               0987654321                                      *
-*               1111111111                                      *
-*               1111111111                                      *
-*               2222222222                                      *
-*                                                               *
-* The pattern array is:                                         *
-*               876543                                          *
-*               111111                                          *
-*               111111                                          *
-*                                                               *
-* The pattern begins at the second row and the third column of  *
-* the grid and continues in the following two rows. The pattern *
-* is said to be present in the grid. The return value should be *
-* YES or NO, depending on whether the pattern is found.         *
-* In this case, return YES.                                     *
-*                                                               *
-* Function Description:                                         *
-* Complete the gridSearch function in the editor below. It      *
-* should return YES if the pattern exists in the grid, or NO    *
-* otherwise. gridSearch has the following parameter(s):         *
-* 1) string G[R]: the grid to search                            *
-* 2) string P[r]: the pattern to search for                     *
-*                                                               *
-* HackerRank Problem URL:                                       *
-* https://www.hackerrank.com/challenges/the-grid-search/problem *
-*                                                               *
-****************************************************************/
+// Hackerrrank: https://www.hackerrank.com/challenges/the-grid-search/
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-        inputString += inputStdin;
-});
+process.stdin.on("data", (inputStdin) => (inputString += inputStdin));
 
-process.stdin.on('end', function() {
-        inputString = inputString.split('\n');
+process.stdin.on("end", () => {
+        inputString = inputString.split("\n");
         main();
 });
 
-function readLine() {
-        return inputString[currentLine++];
-}
+const readLine = () => inputString[currentLine++];
 
-function checkIfPatterExists( grid, row, column, pattern ) {
-        for( let i = 0; i < pattern.length; i++ ) {
-                let sliceString = grid[row].slice( column, column + pattern[0].length );
-                if( pattern[ i ] !== sliceString ) {
+const checkIfPatterExists = (grid, row, column, pattern) => {
+        for (let i = 0; i < pattern.length; i++) {
+                let sliceString = grid[row].slice(column, column + pattern[0].length);
+                if (pattern[i] !== sliceString) {
                         return false;
                 }
                 row++;
         }
         return true;
-}
+};
 
-/** 
- * @param { string[] } G 
- * @param { string[] } P 
- * @returns { string }
-*/
-
-// Complete the 'gridSearch' function below.
-function gridSearch(G, P) {
-        for( let i = 0; i < G.length; i++ ) {
-                for( let j = 0; j < G[i].length; j++ ) {
-                        if( G[i][j] === P[0][0] && checkIfPatterExists( G, i, j, P ) ) {
+const gridSearch = (G, P) => {
+        for (let i = 0; i < G.length; i++) {
+                for (let j = 0; j < G[i].length; j++) {
+                        if (G[i][j] === P[0][0] && checkIfPatterExists(G, i, j, P)) {
                                 return "YES";
                         }
                 }
         }
         return "NO";
-}
+};
 
-function main() {
+const main = () => {
         const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
         const t = parseInt(readLine().trim(), 10);
         for (let tItr = 0; tItr < t; tItr++) {
-                const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+                const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
                 const R = parseInt(firstMultipleInput[0], 10);
                 const C = parseInt(firstMultipleInput[1], 10);
                 let G = [];
@@ -98,7 +53,7 @@ function main() {
                         const GItem = readLine();
                         G.push(GItem);
                 }
-                const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+                const secondMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
                 const r = parseInt(secondMultipleInput[0], 10);
                 const c = parseInt(secondMultipleInput[1], 10);
                 let P = [];
@@ -108,7 +63,7 @@ function main() {
                         P.push(PItem);
                 }
                 const result = gridSearch(G, P);
-                ws.write(result + '\n');
+                ws.write(result + "\n");
         }
         ws.end();
-}
+};
