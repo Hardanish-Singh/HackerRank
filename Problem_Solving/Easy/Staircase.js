1dccc702f806938c7146234ac00d6278dab47e6c
Hardanish Singh <hardanishsingh1@gmail.com>
2023-03-29 07:44:00 -0400

    Staircase

--- Problem_Solving/Easy/Staircase.js
+++ Problem_Solving/Easy/Staircase.js
@@ -1,88 +1,41 @@
-/****************************************************************
-*               HACKERRANK STAIRCASE CHALLENGE	                *
-*                                                               *
-* This is a staircase of size n = 4                             *
-*                                                               *
-*                       #                                       *
-*                      ##                                       *
-*                     ###                                       *
-*                    ####                                       *
-*                                                               *
-* Its base and height are both equal to n, It is drawn using #  *
-* symbols & spaces. The last line is not preceded by any spaces *
-* Write a program that prints a staircase of size n             *
-*                                                               *
-* Function Description: Complete the staircase function in the  *
-* editor below. staircase has the following parameter(s): int n *
-*                                                               *
-* Print                                                         *
-* Print a staircase as described above.                         *
-*                                                               *
-* Input Format                                                  *
-* 1) A single integer, n, denoting the size of the staircase.   *
-*                                                               *
-* Constraints                                                   *
-* 1) 0 <= n <= 100                                              *
-*                                                               *
-* Input 1:                                                      *
-*           6                                                   *
-* Output 1:                                                     *
-*                       #                                       *
-*                      ##                                       *
-*                     ###                                       *
-*                    ####                                       *
-*                   #####                                       *
-*                  ######                                       *
-*                                                               *
-****************************************************************/
+// Hackerrrank: https://www.hackerrank.com/challenges/staircase/
 
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-        inputString += inputStdin;
-});
+process.stdin.on("data", (inputStdin) => (inputString += inputStdin));
 
-process.stdin.on('end', function() {
-        inputString = inputString.split('\n');
+process.stdin.on("end", () => {
+        inputString = inputString.split("\n");
         main();
 });
 
-function readLine() {
-        return inputString[currentLine++];
-}
+const readLine = () => inputString[currentLine++];
 
-/**
- * @param { number } n
- * @returns { }
-*/
-
-// Complete the 'staircase' function below.
-
-function staircase( n ) {
+const staircase = (n) => {
         var spaces = n - 1;
         var hash = 1;
-        for( let i = 1; i <= n; i++ ) {
+        for (let i = 1; i <= n; i++) {
                 // Spaces loop
-                for( let j = 1; j <= spaces; j++ ) {
+                for (let j = 1; j <= spaces; j++) {
                         process.stdout.write(" ");
                 }
                 // Hash Loop
-                for( let j = 1; j <= hash; j++ ) {
+                for (let j = 1; j <= hash; j++) {
                         process.stdout.write("#");
                 }
                 console.log();
                 spaces--;
                 hash++;
         }
-}
+};
 
-function main() {
+const main = () => {
         const n = parseInt(readLine().trim(), 10);
         staircase(n);
-}
+};
